502d27f7bbd6d9b1640f005281407e0324a853bb
Jagm33t <130860323+Jagm33t@users.noreply.github.com>
2023-11-05 02:32:13 -0800

    Backend setup done

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignOut from './pages/SignOut';
+import Singnin from './pages/Singnin';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+function App() {
+  
+
+  return (
+    <BrowserRouter>
+    <Header />
+     <Routes>
+       <Route path='/' element={<Home />} />
+       <Route path='/about' element={<About />} />
+       <Route path='/signout' element={<SignOut />} />
+       <Route path='/signin' element={<Singnin />} />
+       <Route path='/profile' element={<Profile />} />
+     </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
