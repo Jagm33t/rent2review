86c3759e901d0462cbf01d11cdc5b7c1f4156880
Jagm33t <130860323+Jagm33t@users.noreply.github.com>
2023-11-07 02:03:25 -0800

    Google Oauth added and working

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
-
+import OAuth from '../components/OAuth';
 const SignUp = () => {
 
   const [formData, setFormData] = useState({});
@@ -59,6 +59,7 @@ const SignUp = () => {
         onChange={handleChange}/>
         <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase 
         hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Sign up'}</button>
+                <OAuth />
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
--- client/src/pages/Singnin.jsx
+++ client/src/pages/Singnin.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
+import OAuth from '../components/OAuth';
 
 const Signin = () => {
 
@@ -55,6 +56,7 @@ const Signin = () => {
         onChange={handleChange}/>
         <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase 
         hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Sign in'}</button>
+        <OAuth />
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Don't have an account?</p>
