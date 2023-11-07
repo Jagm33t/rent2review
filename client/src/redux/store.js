45bc1e57bb8a3d0baf38097a216d07f8cd80ea7f
Jagm33t <130860323+Jagm33t@users.noreply.github.com>
2023-11-07 00:39:08 -0800

    Persist redux is installed and wrapped

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,25 @@
-import { configureStore, } from '@reduxjs/toolkit';
-import  userReducer  from './user/userSlice';
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux'; // Add this import
+import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistedConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistedConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {user: userReducer},
-  middleware: (getDefaultMiddleware) => 
+  reducer: persistedReducer, // Fix this line
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck:false,
+      serializableCheck: false,
     }),
-})
+});
+
+export const persistor = persistStore(store);
