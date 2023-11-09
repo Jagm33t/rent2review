04d5065d4865d6ba44b030386da84b7956ad7a85
Jagm33t <130860323+Jagm33t@users.noreply.github.com>
2023-11-09 14:13:35 -0800

    User api route done

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,8 +1,8 @@
- const erroHandler = (statusCode, message)=>{
+ const errorHandler = (statusCode, message)=>{
   const error = new Error()
   error.statusCode =statusCode
   error.message = message;
   return error;
 }
 
-export default erroHandler;
+export default errorHandler;
