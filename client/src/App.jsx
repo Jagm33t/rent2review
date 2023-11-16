af2bf354bb142705fceaf48addbaf067ca84210f
Jagm33t <130860323+Jagm33t@users.noreply.github.com>
2023-11-16 15:43:37 -0800

    signin page eeror fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import SignUp from './pages/SignUp';
-import SignIn from './pages/SingnIn';
+import SignIn from './pages/SignIn';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
