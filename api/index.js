4e6e4ad81e15f72d15d286b4dfc620adf7f8e295
Jagm33t <130860323+Jagm33t@users.noreply.github.com>
2023-11-20 16:20:38 -0800

    index.js file update

--- api/index.js
+++ api/index.js
@@ -1,34 +1,30 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
-import listingRouter from './routes/listing.route.js'
-import cookieParser from 'cookie-parser';
-import path from 'path';
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRouter = require('./routes/user.route.js');
+const authRouter = require('./routes/auth.route.js');
+const listingRouter = require('./routes/listing.route.js');
+const cookieParser = require('cookie-parser');
+const path = require('path');
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO)
-.then(()=>{
-  console.log("connected to DATA BASE")
-
-})
-.catch((err) =>{
-  console.log(err);
-})
-
-
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('connected to DATA BASE');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const __dirname = path.resolve();
-
 const app = express();
 
 app.use(express.json());
-
 app.use(cookieParser());
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
 
@@ -36,14 +32,11 @@ app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
-
-
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
-
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
@@ -54,4 +47,3 @@ app.use((err, req, res, next) => {
     message,
   });
 });
- 
