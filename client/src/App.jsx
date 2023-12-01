2f9e6e4d30f26d32c2ab5ae02c27c5ac54027b29
Jagm33t <130860323+Jagm33t@users.noreply.github.com>
2023-12-01 03:20:33 -0800

    Review page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
 import Search from './pages/Search';
+import Review from './pages/Review';
 
 
 function App() {
@@ -33,6 +34,7 @@ function App() {
             element={<UpdateListing />}
           />
         </Route>
+        <Route path='/review' element={<Review />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ client/src/pages/Review.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const Review = () => {
+  return (
+    <div className="bg-white p-6 shadow rounded-lg my-6">
+      <div className="mb-6">
+        <h2 className="text-2xl font-bold text-gray-900">New England Family Housing</h2>
+        <p className="text-gray-600">Claremont, New Hampshire, US, 03743</p>
+        <p className="text-gray-500">11/30/2023</p>
+      </div>
+      <div className="border-t pt-4">
+        <div className="flex justify-between items-center text-gray-600 mb-2">
+          <span>Health and Safety:</span>
+          <span className="font-medium">★★☆☆☆</span>
+        </div>
+        <div className="flex justify-between items-center text-gray-600 mb-2">
+          <span>Respect:</span>
+          <span className="font-medium">★★★☆☆</span>
+        </div>
+        <div className="flex justify-between items-center text-gray-600 mb-2">
+          <span>Tenant Privacy:</span>
+          <span className="font-medium">★★☆☆☆</span>
+        </div>
+        <div className="flex justify-between items-center text-gray-600 mb-2">
+          <span>Repair:</span>
+          <span className="font-medium">★★★☆☆</span>
+        </div>
+        <div className="flex justify-between items-center text-gray-600">
+          <span>Rental Stability:</span>
+          <span className="font-medium">★★★☆☆</span>
+        </div>
+      </div>
+      <div className="mt-6">
+        <p className="text-gray-700">
+          Absolute garbage rentals run by absolute garbage humans. Raising my rent by another
+          $100 despite the fact that there are 3 people currently in a 1 bedroom and I've
+          repeatedly asked them to transfer us to a larger unit...
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Review;
