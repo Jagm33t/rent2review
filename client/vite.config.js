1fffba73847daccdfc1afd20e3373ca04c01be56
Jagm33t <130860323+Jagm33t@users.noreply.github.com>
2024-01-07 13:42:52 -0800

    tab name chnaged

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     host: '0.0.0.0',  // Add this line to make the server accessible over the local network
     proxy: {
       '/api': {
-        target: 'mern-test-app.azurewebsites.net',
+        target: 'http://localhost:3000',
         secure: false,
       },
     },
